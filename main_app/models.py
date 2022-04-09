797b857960904683934be9fe813df4f887be4ad1
neshmasood <neshtam@gmail.com>
2022-04-08 22:08:08 -0500

    create Task model

--- main_app/models.py
+++ main_app/models.py
@@ -1,1 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
+
+
+STATUS_CHOICES = {
+    ("Not yet started", "not yet started"), 
+    ("Completed", "complete"), 
+    ("Incomplete", "incomplete")
+}
+
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    due_date = models.DateTimeField()
+    description = models.CharField(max_length=200)
+    task_status = models.CharField(max_length=20, choices = STATUS_CHOICES)
+    task_approval = models.BooleanField()
+   
+    
+
+    def __str__(self):
+        return self.name
+        # return str(self.item) + ": $" + str(self.price)
+
+    class Meta: 
+        ordering = ['name']
