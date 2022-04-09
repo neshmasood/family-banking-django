b819284c4f281b4fa0c1e71f7d9b83fd328484dd
neshmasood <neshtam@gmail.com>
2022-04-09 13:42:47 -0500

    create New user model to use instead of the default django user model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
-from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User, AbstractUser, AbstractBaseUser, PermissionsMixin
+from django.utils import timezone   
 
 
 # Create your models here.
@@ -73,6 +74,23 @@ class User(AbstractUser):
     user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
     tasks = models.ManyToManyField(Task)
     transactions = models.ManyToManyField(Transaction)
+
+
+
+class NewUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    user_name = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    start_date = models.DateTimeField(default=timezone.now)
+    about = models.TextField(_(
+        'about'), max_length=500, blank=True)
+    is_child = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+   
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name', 'first_name']
+    def __str__(self):
+        return self.user_name
 
 
 
