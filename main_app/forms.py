680933e38617a50496ec895ec6e6d45dc3cdf530
neshmasood <neshtam@gmail.com>
2022-04-10 22:16:31 -0500

    create: forms for child and parent users signup

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,2 +1,49 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django import forms
+from .models import ChildUser
+from .models import ParentUser
+
+
+
+
+
+# class SignUpForm(UserCreationForm):
+#     first_name = forms.CharField(max_length=30)
+#     last_name = forms.CharField(max_length=50)
+#     email = forms.EmailField()
+#     class Meta:
+#         model = User
+#         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2' )
+
+
+
+# # User Signup Form common for both parent and child login
+# class SignUpForm(UserCreationForm):
+#     class Meta:
+#         model = User
+#         fields = ('username', 'first_name', 'last_name', 'email', 'is_parent', 'is_child', 'password1', 'password2' )
+      
+# Parent SignUp Form 
+class ParentSignUpForm(forms.Form):
+    class Meta():
+        model = ParentUser
+    class Meta:
+        model = ParentUser
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        widgets = {
+                'unique_family_name': forms.TextInput(attrs={'unique-family-name':'answer'})
+                }
+# Child SignUp Form
+class ChildSignUpForm(forms.Form):
+    # class Meta():
+    #     model = ChildUser
+    email = forms.EmailField(max_length=250)
+    username = forms.CharField(max_length=50)
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    family_key = forms.CharField(max_length=100)
+    class Meta:
+        model = ChildUser
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2' 'family_key')
+        # widgets = {
+        #         'unique_family_name': forms.TextInput(attrs={'unique-family-name':'answer'})
+        #         }
