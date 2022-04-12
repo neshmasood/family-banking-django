bf0e240ac9dfef1eebfd811c43dd84145301cea8
neshmasood <neshtam@gmail.com>
2022-04-12 00:07:32 -0500

    scaled down to single user for app. re-create the models, setup all urls, views, forms, and update the admin and base.html.

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,39 +1,58 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
-# from .models import ChildUser, ParentUser
-# from .models import ParentUser
 from django.contrib.auth.models import User
+from django import forms
 
 
-      
-# Parent SignUp Form 
-class ParentSignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=250)
-    username = forms.CharField(max_length=50)
-    first_name = forms.CharField(max_length=50)
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=50)
+    email = forms.EmailField()
     family_key = forms.CharField(max_length=100)
-   
-   
+
+
+
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'family_key')
-        # widgets = {
-        #         'family_key': forms.TextInput(attrs={'family_key':'answer'})
-        #         }
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'family_key' )
 
 
-# Child SignUp Form
-class ChildSignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=250)
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
-    family_key = forms.CharField(max_length=100)
+# from django import forms
+# from django.contrib.auth.forms import UserCreationForm
+# # from .models import ChildUser, ParentUser
+# # from .models import ParentUser
+# from django.contrib.auth.models import User
+
+
+# # Parent SignUp Form 
+# class ParentSignUpForm(UserCreationForm):
+#     email = forms.EmailField(max_length=250)
+#     username = forms.CharField(max_length=50)
+#     first_name = forms.CharField(max_length=50)
+#     last_name = forms.CharField(max_length=50)
+#     family_key = forms.CharField(max_length=100)
+   
+   
+#     class Meta:
+#         model = ParentUser
+#         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'family_key')
+#         # widgets = {
+#         #         'family_key': forms.TextInput(attrs={'family_key':'answer'})
+#         #         }
+
+
+# # Child SignUp Form
+# class ChildSignUpForm(UserCreationForm):
+#     email = forms.EmailField(max_length=250)
+#     first_name = forms.CharField(max_length=50)
+#     last_name = forms.CharField(max_length=50)
+#     family_key = forms.CharField(max_length=100)
     
 
-    class Meta:
-        model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'family_key')
-        # widgets = {
-        #         'family_key': forms.TextInput(attrs={'family_key':'answer'})
-        #         }
+#     class Meta:
+#         model = User
+#         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'family_key')
+#         # widgets = {
+#         #         'family_key': forms.TextInput(attrs={'family_key':'answer'})
+#         #         }
